26b722035d947d18eb40bd9961b1d0c04c9c6005
Shrikrushana <shrikrushana@maukaa.io>
2025-10-09 13:37:24 +0530

    added fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss(),react()],
+  plugins: [tailwindcss(), react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // remove /api prefix if needed
+      }
+    }
+  }
 })
